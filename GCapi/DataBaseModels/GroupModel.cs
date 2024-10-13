62f9689a72c2c7751fc148cb76252463b12ffb69
Alexlender <sashacharugin@mail.ru>
2024-10-13 21:30:20 +0700

    GroupModel fixes

--- GCapi/DataBaseModels/GroupModel.cs
+++ GCapi/DataBaseModels/GroupModel.cs
@@ -1,10 +1,12 @@
-﻿namespace gcapi.Models
+﻿using gcapi.DataBaseModels;
+
+namespace gcapi.Models
 {
     public class GroupModel
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
-        public List<string> GroupUsersLogins { get; set; }
-        public List<EventModel> GroupEvents { get; set; }
+        public required string Name { get; set; }
+        public List<UserModel> GroupUsers { get; set; } = [];
+        public List<EventModel> GroupEvents { get; set; } = [];
     }
 }
