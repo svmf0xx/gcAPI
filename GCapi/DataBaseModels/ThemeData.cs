abe7e59f87a7497c2c4dad8202118cef6399ef38
Alexlender <sashacharugin@mail.ru>
2024-10-13 19:05:04 +0700

    hello commit

--- GCapi/DataBaseModels/ThemeData.cs
+++ GCapi/DataBaseModels/ThemeData.cs
@@ -4,5 +4,7 @@
     {
         public string Color { get; set; } = "white";
         public string PicturePath { get; set; } = "/etc/themepics/vlad.png"; // хмм
+        //а зачем? Это типа настройки внешнего вида, которые вообще могут храниться в куки, а не в бд
+        //плюс тут не должна храниться аватарка пользователя
     }
 }
