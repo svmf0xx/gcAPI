819059d94f8980bf3f7e5783266f9f44151d6794
Alexlender <sashacharugin@mail.ru>
2024-10-15 18:43:00 +0700

    certs

--- GCapi/Program.cs
+++ GCapi/Program.cs
@@ -1,9 +1,15 @@
 using gcapi.DataBase;
 using gcapi.Interfaces.Services;
 using gcapi.Realizations;
+using Microsoft.AspNetCore.Authentication.Certificate;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using Microsoft.AspNetCore.Server.Kestrel.Https;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.OpenApi.Models;
+using System.Configuration;
 using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -38,6 +44,21 @@ builder.Services.AddTransient<IUserService, UserService>();
 builder.Services.AddTransient<IGroupService, GroupService>();
 
 
+builder.Services.AddAuthentication(
+    CertificateAuthenticationDefaults.AuthenticationScheme)
+    .AddCertificate(options =>
+        options.CustomTrustStore = new X509Certificate2Collection(new X509Certificate2(
+        builder.Configuration.GetSection("Cert")["File"],
+        builder.Configuration.GetSection("Cert")["Password"]))
+    );
+
+builder.Services.Configure<KestrelServerOptions>(options =>
+{
+    options.ConfigureHttpsDefaults(options =>
+        options.ClientCertificateMode = ClientCertificateMode.RequireCertificate);
+
+});
+
 var app = builder.Build();
 
 
@@ -51,6 +72,8 @@ app.UseSwaggerUI(c =>
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseAuthentication();
 
 app.MapControllers();
 
