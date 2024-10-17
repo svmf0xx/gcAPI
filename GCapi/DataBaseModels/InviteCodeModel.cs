7542eaa7caf9906fd4d82954a1d020c3807e489f
Alexlender <sashacharugin@mail.ru>
2024-10-17 20:09:10 +0700

    hmmmm

---
+++ GCapi/DataBaseModels/InviteCodeModel.cs
@@ -0,0 +1,15 @@
+﻿using gcapi.Models;
+
+namespace gcapi.Models
+{
+    public class InviteCodeModel
+    {
+        public Guid Id { get; set; }
+        public UserModel Owner { get; set; }
+        public GroupModel Group { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public DateTime ExspiredAt { get; set; }
+    }
+}
