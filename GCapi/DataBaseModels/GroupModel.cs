1e7306dcb28cc057a9ebd47c8db9394b82b216fa
Alexlender <sashacharugin@mail.ru>
2024-10-17 15:02:19 +0700

    Very important note

--- GCapi/DataBaseModels/GroupModel.cs
+++ GCapi/DataBaseModels/GroupModel.cs
@@ -9,6 +9,7 @@ namespace gcapi.Models
         public long? TgId { get; set; } //пу-пу-пу... че-то поздно догадался это добавить
         public List<UserModel> GroupUsers { get; set; } = [];
         public List<EventModel> GroupEvents { get; set; } = [];
-        public List<PlanModel> GroupPlans { get; set; } = [];
+        public List<PlanModel> GroupPlans { get; set; } = []; //так, стоп
+                                                              //А нахуя тут это?
     }
 }
